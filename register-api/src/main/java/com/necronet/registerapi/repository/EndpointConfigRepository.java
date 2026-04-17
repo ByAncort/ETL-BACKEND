62155cb3aa7f6a019aea659d13c221651c351eff
ByAncort <diego2005cortes@gmail.com>
2026-04-16 22:41:48 -0400

    chore: eliminar microservicio register-api redundante

--- register-api/src/main/java/com/necronet/registerapi/repository/EndpointConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.necronet.registerapi.repository;
-
-import com.necronet.registerapi.entity.EndpointConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EndpointConfigRepository extends JpaRepository<EndpointConfig, Long> {
-}
