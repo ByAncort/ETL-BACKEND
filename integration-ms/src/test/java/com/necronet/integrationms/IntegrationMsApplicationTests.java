15478f830420e95ddcd10fb3e434074ecfc716c3
ByAncort <diego2005cortes@gmail.com>
2026-06-14 23:45:23 -0400

    test: add H2 dependencies and test configuration for all services

--- integration-ms/src/test/java/com/necronet/integrationms/IntegrationMsApplicationTests.java
+++ integration-ms/src/test/java/com/necronet/integrationms/IntegrationMsApplicationTests.java
@@ -2,8 +2,20 @@ package com.necronet.integrationms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "eureka.client.enabled=false",
+    "spring.cloud.discovery.enabled=false",
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "api.register.ms.url=http://localhost:8083",
+    "api.matcher-ms.url=http://localhost:8000"
+})
 class IntegrationMsApplicationTests {
 
     @Test
